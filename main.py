c5680565b46a81cc4d61fc051f83355ca31ff991
omer60901 <prprbtymhmzkyr@gmail.com>
2026-09-14 17:27:48 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+def secondLargest(numbers):
+    unique_numbers = list(set(numbers))
+
+    if len(unique_numbers) < 2:
+        return None
+
+    unique_numbers.sort()
+    return unique_numbers[-2]
+
+def Rectangle(height,width):
+    return (height*width)
+
+
+def main():
+    my_list = [10, 20, 4, 45, 99, 99, 4]
+    print(secondLargest(my_list))
+    print(Rectangle(10,10))
+
+
+if __name__ == "__main__":
+    main()
