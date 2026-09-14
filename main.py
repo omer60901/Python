e208bb211b7529b8a0e02a7666b5f1ccb6fd2cd6
omer60901 <prprbtymhmzkyr@gmail.com>
2026-09-14 17:51:55 +0300

    Update message in main

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ def Rectangle(height,width):
 
 def main():
     my_list = [10, 20, 4, 45, 99, 99, 4]
+    print("Second largest number:")
     print(secondLargest(my_list))
     print(Rectangle(10,10))
+
 
 
 if __name__ == "__main__":
